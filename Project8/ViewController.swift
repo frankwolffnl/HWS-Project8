efd120b7c156a86e0e735a6f7330411576cd7347
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-06 20:23:19 +0100

    loadLevel() method implemented.

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -7,17 +7,72 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
+    override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+        
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
 	}
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePatch = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePatch) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+                
+                for (index, line) in lines.enumerated() {
+					let parts = line.components(separatedBy: ": ")
+					let answer = parts[0]
+					let clue = parts[1]
+					
+					clueString += "\(index + 1). \(clue)\n"
+					
+					let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+					solutionString += "\(solutionWord.count) letters\n"
+					solutions.append(solutionWord)
+					
+					let bits = answer.components(separatedBy: "|")
+					letterBits += bits
+                }
+            }
+        }
+    }
 
-	override func didReceiveMemoryWarning() {
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
+    @objc func letterTapped(btn: UIButton) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
 
